10e1ec65daf8d941acad0d583906aebe34aa1437
Marcelo11114 <dedosearte@hotmail.com>
2020-09-13 14:51:05 -0300

    Caixa Nome, coisa chata , pegar pra fazer depois pelo menos o nme piscando como letreiro

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CaixaNome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" +------------------+");
+            Console.WriteLine(" | Valdecir Marcelo |");
+            Console.WriteLine(" +------------------+");
+        }
+    }
+}
